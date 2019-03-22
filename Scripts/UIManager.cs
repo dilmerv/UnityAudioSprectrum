ddc7b2a91683896604f4ac35d638ca6872de458f
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-22 01:13:56 -0600

    Initial project files

---
+++ Scripts/UIManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Slider timeSpeedSlider = null;
+
+    [SerializeField]
+    private Text timeSpeedSliderLabel = null;
+
+    [SerializeField]
+    private Slider noiseScaleSlider = null;
+
+    [SerializeField]
+    private Text noiseScaleSliderLabel = null;
+
+    [SerializeField]
+    private Material deformationMaterial = null;
+
+    [SerializeField]
+    private bool useSamplesFromAudio = false;
+
+    private SpectrumManager spectrumManager = null;
+
+    [SerializeField]
+    private float sampleMultipler = 100.0f;
+
+    [SerializeField]
+    private float delayBy = 1.0f;
+    private float elapsedTime = 0;
+
+    void Start() 
+    {
+        spectrumManager = SpectrumManager.Instance;
+
+        timeSpeedSlider.onValueChanged.AddListener((value) => timeSpeedSliderChanged(value));
+        noiseScaleSlider.onValueChanged.AddListener((value) => noiseScaleSliderChanged(value));
+    }
+    
+    void Update()
+    {
+        if(deformationMaterial != null)
+        {
+            if(useSamplesFromAudio)
+            {
+                float value = spectrumManager.Samples[0] * sampleMultipler;
+                float clampValue = Mathf.Clamp(value, 0.1f, 100.0f);
+
+                if(elapsedTime >= delayBy)
+                {
+                    noiseScaleSliderChanged(clampValue);
+                    elapsedTime = 0;
+                }
+                else {
+                    elapsedTime += Time.deltaTime * 1.0f;
+                    Debug.Log(elapsedTime);
+                }
+            }
+        }
+    }
+
+    void timeSpeedSliderChanged(float newValue) 
+    {
+        timeSpeedSlider.value = newValue;
+        deformationMaterial.SetFloat("_TimeSpeed", timeSpeedSlider.value);
+        timeSpeedSliderLabel.text = $"Time Speed: {timeSpeedSlider.value}";
+    }
+
+     void noiseScaleSliderChanged(float newValue) 
+     {
+        noiseScaleSlider.value = newValue;
+        deformationMaterial.SetFloat("_NoiseScale", noiseScaleSlider.value);
+        noiseScaleSliderLabel.text = $"Noise Scale: {noiseScaleSlider.value}";
+    }
+}
---
+++ SpectrumManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityLWRPEssentials.Assets.Scripts.Core;
+
+public class SpectrumManager : MonoBehaviourSingleton<SpectrumManager>
+{
+    [SerializeField]
+    private AudioSource audioSource = null;
+
+    [SerializeField]
+    private float[] samples = new float[512];
+
+    public float[] Samples
+    {
+        get 
+        {
+            return this.samples;
+        }
+    }
+
+    void Update() => audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+}
